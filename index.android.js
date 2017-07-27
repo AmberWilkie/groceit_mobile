87a5b089e7a97620e4d204f52843bde9ed1537a4
Amber Wilkie <amber@amberwilkie.com>
2017-07-27 15:24:56 +0200

    Initial commit with counter

---
+++ index.android.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import groceit_mobile from './src/App';
+
+AppRegistry.registerComponent('groceit_mobile', () => groceit_mobile);
